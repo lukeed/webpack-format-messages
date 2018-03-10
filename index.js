820d6186f84dae88302c90905ff145760939e9ed
Rich Harris <richard.a.harris@gmail.com>
2018-03-10 15:11:45 -0500

    replace chalk with clorox (#3)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-const chalk = require('chalk');
+const clorox = require('clorox');
 
 const errorLabel = 'Syntax error:';
 const isLikelyASyntaxError = str => str.includes(errorLabel);
@@ -55,7 +55,7 @@ function formatMessage(message, isError) {
     lines[1] = lines[1].replace(exportRegex, "$1 '$4' does not contain an export named '$3'.");
   }
 
-  lines[0] = chalk.inverse(lines[0]);
+  lines[0] = clorox.inverse(lines[0]);
 
   // Reassemble & Strip internal tracing, except `webpack:` -- (create-react-app/pull/1050)
   return lines.join('\n').replace(stackRegex, '').trim();
