3841b0ff0191f718d411b33b7aefa511e07bde63
Nolan Lawson <nolan@nolanlawson.com>
2018-12-01 16:17:02 -0800

    fix: undefined colors error (#6)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * see: https://github.com/facebookincubator/create-react-app/tree/master/packages/react-dev-utils
  */
 
-const { inverse } = require('kleur');
+const colors = require('kleur');
 
 const errorLabel = 'Syntax error:';
 const isLikelyASyntaxError = str => str.includes(errorLabel);
@@ -53,7 +53,7 @@ function formatMessage(message, isError) {
     lines[1] = lines[1].replace(exportRegex, "$1 '$4' does not contain an export named '$3'.");
   }
 
-  lines[0] = inverse(lines[0]);
+  lines[0] = colors.inverse(lines[0]);
 
   // Reassemble & Strip internal tracing, except `webpack:` -- (create-react-app/pull/1050)
   return lines.join('\n').replace(stackRegex, '').trim();
