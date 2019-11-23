34baf4ca7171d8ee6d448de88cae1af40f864fbf
Luke Edwards <luke.edwards05@gmail.com>
2019-11-23 13:54:51 -0800

    fix: handle object w/ message fallback

--- index.js
+++ index.js
@@ -12,61 +12,58 @@ const exportRegex = /\s*(.+?)\s*(")?export '(.+?)' was not found in '(.+?)'/;
 const stackRegex = /^\s*at\s((?!webpack:).)*:\d+:\d+[\s\)]*(\n|$)/gm;
 
 function formatMessage(message, isError) {
-	// Webpack v5 gives us an object, not a string, but message.details is
-	// what we are interested in here.
-	if (typeof message === "object") {
-		if (typeof message.details === "string") {
-			message = message.details;
-		} else if (typeof message.stack === "string") {
-			message = message.stack;
-		}
+	// Workaround to accommodate Webpack v5
+	// It gives us an Object now, not a string...
+	// Objects not identical; details > stack > message
+	if (typeof message === 'object') {
+		message = message.details || message.stack || message.message;
 	}
 
-  let lines = message.split('\n');
+	let lines = message.split('\n');
 
-  if (lines.length > 2 && lines[1] === '') {
-    lines.splice(1, 1); // Remove extra newline.
-  }
+	if (lines.length > 2 && lines[1] === '') {
+		lines.splice(1, 1); // Remove extra newline.
+	}
 
-  // Remove loader notation from filenames:
-  //   `./~/css-loader!./src/App.css` ~~> `./src/App.css`
-  if (lines[0].lastIndexOf('!') !== -1) {
-    lines[0] = lines[0].substr(lines[0].lastIndexOf('!') + 1);
-  }
+	// Remove loader notation from filenames:
+	//   `./~/css-loader!./src/App.css` ~~> `./src/App.css`
+	if (lines[0].lastIndexOf('!') !== -1) {
+		lines[0] = lines[0].substr(lines[0].lastIndexOf('!') + 1);
+	}
 
 	// Remove useless `entry` filename stack details
-  lines = lines.filter(line => line.indexOf(' @ ') !== 0);
+	lines = lines.filter(line => line.indexOf(' @ ') !== 0);
 
-  // 0 ~> filename; 1 ~> main err msg
-  if (!lines[0] || !lines[1]) {
-    return lines.join('\n');
-  }
+	// 0 ~> filename; 1 ~> main err msg
+	if (!lines[0] || !lines[1]) {
+		return lines.join('\n');
+	}
 
-  // Cleans up verbose "module not found" messages for files and packages.
-  if (lines[1].startsWith('Module not found: ')) {
-    lines = [
-      lines[0],
-      lines[1] // "Module not found: " is enough detail
-        .replace("Cannot resolve 'file' or 'directory' ", '')
-        .replace('Cannot resolve module ', '')
-        .replace('Error: ', '')
-        .replace('[CaseSensitivePathsPlugin] ', '')
-    ];
-  }
+	// Cleans up verbose "module not found" messages for files and packages.
+	if (lines[1].startsWith('Module not found: ')) {
+		lines = [
+			lines[0],
+			lines[1] // "Module not found: " is enough detail
+				.replace("Cannot resolve 'file' or 'directory' ", '')
+				.replace('Cannot resolve module ', '')
+				.replace('Error: ', '')
+				.replace('[CaseSensitivePathsPlugin] ', '')
+		];
+	}
 
-  // Cleans up syntax error messages.
-  if (lines[1].startsWith('Module build failed: ')) {
-    lines[1] = lines[1].replace('Module build failed: SyntaxError:', errorLabel);
-  }
+	// Cleans up syntax error messages.
+	if (lines[1].startsWith('Module build failed: ')) {
+		lines[1] = lines[1].replace('Module build failed: SyntaxError:', errorLabel);
+	}
 
-  if (lines[1].match(exportRegex)) {
-    lines[1] = lines[1].replace(exportRegex, "$1 '$4' does not contain an export named '$3'.");
-  }
+	if (lines[1].match(exportRegex)) {
+		lines[1] = lines[1].replace(exportRegex, "$1 '$4' does not contain an export named '$3'.");
+	}
 
-  lines[0] = colors.inverse(lines[0]);
+	lines[0] = colors.inverse(lines[0]);
 
-  // Reassemble & Strip internal tracing, except `webpack:` -- (create-react-app/pull/1050)
-  return lines.join('\n').replace(stackRegex, '').trim();
+	// Reassemble & Strip internal tracing, except `webpack:` -- (create-react-app/pull/1050)
+	return lines.join('\n').replace(stackRegex, '').trim();
 }
 
 module.exports = function (stats) {
@@ -78,16 +75,16 @@ module.exports = function (stats) {
 	};
 
 	// Only show syntax errors if we have them
-  if (result.errors.some(isLikelyASyntaxError)) {
-    result.errors = result.errors.filter(isLikelyASyntaxError);
-  }
+	if (result.errors.some(isLikelyASyntaxError)) {
+		result.errors = result.errors.filter(isLikelyASyntaxError);
+	}
 
-  // First error is usually it; others usually the same
-  if (result.errors.length > 1) {
-    result.errors.length = 1;
-  }
+	// First error is usually it; others usually the same
+	if (result.errors.length > 1) {
+		result.errors.length = 1;
+	}
 
-  return result;
+	return result;
 };
 
 module.exports.formatMessage = formatMessage;
